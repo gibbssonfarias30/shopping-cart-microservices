1c0cf01efd29985c127d88a6aaadbc2e6996b801
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-06 00:45:00 -0500

    chore: add import.sql with initial seed data

---
+++ product-service/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO category (name) VALUES('Electronico');
+INSERT INTO category (name) VALUES('Libros');
+INSERT INTO category (name) VALUES('Zapatos');
+
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Laptop', 10, 800, NOW(), 'NEW','CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Monitor', 10, 700, NOW(), 'NEW', 'CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Mouse', 10, 1000, NOW(), 'NEW','CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Old Mouse', 10, 1000, NOW(), 'NEW', 'CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Product 5', 10, 1000, NOW(), 'NEW', 'CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Product 6', 10, 1000, NOW(), 'NEW', 'CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Product 7', 10, 1000, NOW(), 'NEW', 'CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Product 8', 10, 1000, NOW(), 'NEW', 'CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Product 9', 10, 1000, NOW(), 'NEW', 'CREATED', 1);
+INSERT INTO product (name, stock, price, created_date, status, deleted, category_id) VALUES('Product 10', 10, 1000, NOW(), 'NEW', 'CREATED', 1);
