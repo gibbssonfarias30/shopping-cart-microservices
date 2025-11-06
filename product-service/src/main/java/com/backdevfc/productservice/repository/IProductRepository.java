d6be58b66b74cf514b6fadeeb37253213a0fcd14
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-06 00:30:18 -0500

    feat: add ProductRepository

---
+++ product-service/src/main/java/com/backdevfc/productservice/repository/IProductRepository.java
@@ -0,0 +1,21 @@
+package com.backdevfc.productservice.repository;
+
+import com.backdevfc.productservice.model.entity.CategoryEntity;
+import com.backdevfc.productservice.model.entity.ProductEntity;
+import com.backdevfc.productservice.model.enums.DeletedProduct;
+import com.backdevfc.productservice.model.enums.ProductStatus;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+
+public interface IProductRepository extends CrudRepository<ProductEntity, Long> {
+
+    @Query("FROM ProductEntity p WHERE p.deleted = :deleted AND (:status IS NULL OR p.status = :status)")
+    List<ProductEntity> findAll(@Param("deleted") DeletedProduct deleted, @Param("status") ProductStatus status);
+
+    List<ProductEntity> findByCategoryAndDeleted(CategoryEntity category, DeletedProduct deleted);
+
+}
