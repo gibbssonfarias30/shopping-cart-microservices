8123dd342d456059957cccd5205e9833b2d6210f
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-09 09:14:55 -0500

    feat: add global exception handler and custom exceptions

---
+++ cart-service/src/main/java/com/backdevfc/cartservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.backdevfc.cartservice.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResourceNotFoundException extends RuntimeException {
+
+}
