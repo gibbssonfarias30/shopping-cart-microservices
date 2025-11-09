8ab64ba4636576e1de15d7ca4d357a0ef7b96886
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-09 10:21:34 -0500

    feat: add project discovery-server

--- discovery-server/src/test/java/com/backdevfc/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/backdevfc/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.backdevfc.cartservice;
+package com.backdevfc.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CartServiceApplicationTests {
+class DiscoveryServerApplicationTests {
 
     @Test
     void contextLoads() {
