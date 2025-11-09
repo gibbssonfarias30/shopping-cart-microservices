5181188eede836d347a9aaa7592a8e724f77ccac
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-09 09:05:26 -0500

    chore: modified extends JpaRepository

--- product-service/src/main/java/com/backdevfc/productservice/repository/IProductRepository.java
+++ product-service/src/main/java/com/backdevfc/productservice/repository/IProductRepository.java
@@ -4,14 +4,14 @@ import com.backdevfc.productservice.model.entity.CategoryEntity;
 import com.backdevfc.productservice.model.entity.ProductEntity;
 import com.backdevfc.productservice.model.enums.DeletedProduct;
 import com.backdevfc.productservice.model.enums.ProductStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
-public interface IProductRepository extends CrudRepository<ProductEntity, Long> {
+public interface IProductRepository extends JpaRepository<ProductEntity, Long> {
 
     @Query("FROM ProductEntity p WHERE p.deleted = :deleted AND (:status IS NULL OR p.status = :status)")
     List<ProductEntity> findAll(@Param("deleted") DeletedProduct deleted, @Param("status") ProductStatus status);
