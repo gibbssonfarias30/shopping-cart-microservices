3b0d0f48969317e914303968fc3f106f91db76e0
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-09 09:08:08 -0500

    feat: add ProductFeignClient interface for external product service integration

---
+++ cart-service/src/main/java/com/backdevfc/cartservice/client/ClientProductFeign.java
@@ -0,0 +1,19 @@
+package com.backdevfc.cartservice.client;
+
+import com.backdevfc.cartservice.model.dto.response.ProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "product-service", url = "http://localhost:8001", path = "/api/v1/products")
+public interface ClientProductFeign {
+	
+	@GetMapping
+	public List<ProductResponse> findAll();
+	
+	@GetMapping("/{id}")
+	public ProductResponse findById(@PathVariable Long id);
+
+}
