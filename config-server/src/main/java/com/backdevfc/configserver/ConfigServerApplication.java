d0d215bc317921e6eea2a533f18a967c8f06aa87
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-10 08:39:57 -0500

    feat: enable Config Server and externalize configuration using environment variables

--- config-server/src/main/java/com/backdevfc/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/backdevfc/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.backdevfc.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
