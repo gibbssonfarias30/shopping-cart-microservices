ac6b5e1c56212e39755715f026db47bff63244ca
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-10 10:25:19 -0500

    feat: add project gateway-server

---
+++ gateway-server/src/test/java/com/backdevfc/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backdevfc.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
