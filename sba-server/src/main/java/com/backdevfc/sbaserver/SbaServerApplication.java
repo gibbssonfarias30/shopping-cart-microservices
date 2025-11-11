0247e0e62cad8ac7d93cc5d40b79435bef2b4fee
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-11-11 10:37:44 -0500

    feat: add project sba-server

---
+++ sba-server/src/main/java/com/backdevfc/sbaserver/SbaServerApplication.java
@@ -0,0 +1,13 @@
+package com.backdevfc.sbaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaServerApplication.class, args);
+	}
+
+}
